bac08759122541f87e9d25be9d8b6870ade4a181
Nexus <amitev03@gmail.com>
2016-01-31 06:00:27 +0200

    Update

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e5/20b9dd06cfc70015163ee78e70ec8756
@@ -0,0 +1,8 @@
+package oop;
+
+public class AirConditioner extends HomeTech {
+	private int bTUs;
+	private int dbs;
+	private boolean isSplit;
+	
+}
--- Emag/Emag/oop/AirConditioner.java
+++ Emag/Emag/oop/AirConditioner.java
@@ -5,4 +5,33 @@ public class AirConditioner extends HomeTech {
 	private int dbs;
 	private boolean isSplit;
 	
+	public AirConditioner(int bTUs, int dbs, boolean isSplit) {
+		setbTUs(bTUs);
+		setDbs(dbs);
+		this.isSplit = isSplit;
+	}
+
+	public int getbTUs() {
+		return bTUs;
+	}
+
+	public void setbTUs(int bTUs) {
+		if (bTUs > 0 && bTUs < 99999){
+		this.bTUs = bTUs;
+		}
+	}
+
+	public int getDbs() {
+		return dbs;
+	}
+
+	public void setDbs(int dbs) {
+		if(dbs > 0 && dbs < 200){
+		this.dbs = dbs;
+		}
+	}
+
+	public boolean isSplit() {
+		return isSplit;
+	}	
 }
